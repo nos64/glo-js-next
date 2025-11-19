23ff363ae3f1bc07bcd4c7eef80580cff090db5e
Mikhail Nosov <manosov1984@gmail.com>
2025-11-19 10:19:58 +0300

    feat: add price filter

--- src/modules/filters.js
+++ src/modules/filters.js
@@ -5,3 +5,25 @@ export const searchFilter = (goods, value) => {
 export const categoryFilter = (goods, value) => {
   return goods.filter(good => good.category.toLowerCase().includes(value.toLowerCase()));
 };
+
+export const priceFilter = (goods, min, max) => {
+  return goods.filter(good => {
+    if (min === '' && max === '') {
+
+      return good;
+    } else if (min !== '' && max !== '') {
+
+      return good.price >= +min && good.price <= +max;
+    } else if (min !== '' && max === '') {
+
+      return good.price >= +min;
+    } else if (min === '' && max !== '') {
+
+      return good.price <= +max;
+    }
+  });
+};
+
+export const hotSaleFilter = (goods, value) => {
+  return goods.filter(good => value ? good.sale : good);
+};
